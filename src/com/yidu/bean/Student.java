5477e1199885ea15189e05940eec0b845135dadb
tutu <1194584163@qq.com>
2020-12-24 15:21:44 +0800

    第一次

---
+++ src/com/yidu/bean/Student.java
@@ -0,0 +1,13 @@
+package com.yidu.bean;
+
+/**
+ * @author 刘佳佳
+ * @version 1.0
+ * @公司 一度教育
+ * @date 2020/12/24 15:19
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
